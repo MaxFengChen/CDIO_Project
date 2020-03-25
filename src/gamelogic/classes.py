40b18bfb6ad087b732ab67f3ef3e2cf7d32abfd0
mj23000 <markusjacobsen@hotmail.com>
2020-03-25 21:03:32 +0100

    Added the setupTable() and printCards functions

--- src/gamelogic/classes.py
+++ src/gamelogic/classes.py
@@ -1,9 +1,15 @@
 from enum import Enum 
+import random
+import sys
+
+NOCARDS = 52
+NOCARDS_PLATEAU = 28 # 1+2+3+4+5+6+7=28
+cards = []
 
 class Suit(Enum):
     HEARTS = 0
-    DIAMONDS = 1
-    CLUBS = 2
+    CLUBS = 1
+    DIAMONDS = 2
     SPADES = 3
 
 class Color(Enum):
@@ -42,9 +48,44 @@ class Playing_Card:
         self.pile = pile
         self.value = value
         self.visible = visible
+
+def setupTable():
+    colorSelect = 0
     
-card = Playing_Card(Suit.HEARTS, Color.RED, Pile.STOCK, Value.SEVEN, Visible.TRUE)
+    # Generate a complete deck.
+    for suitSelect in range(4):
+        for valueSelect in range(1, 14):
+            cards.append(Playing_Card(Suit(suitSelect), Color(colorSelect), Pile.STOCK, Value(valueSelect), Visible.FALSE))
 
-print(card.suit, card.color, card.pile, card.value, card.visible)
+        # Toggle the color.
+        colorSelect+=1
+        if colorSelect == 2:
+            colorSelect = 0
+
+    # Shuffle the deck.
+    random.shuffle(cards)
+
+    # Make first 28 cards the playing cards in the plateau.
+    for card in range(NOCARDS_PLATEAU):
+        cards[card].pile = Pile.TABLEAU
+
+    # Make the first seven cards the visible plateau cards.
+    for card in range(7):
+        cards[card].visible = Visible.TRUE
+
+    # Make the rest of the cards the playing cards in the stockpile.
+    for card in range(NOCARDS_PLATEAU, NOCARDS):
+        cards[card].pile = Pile.STOCK
 
 
+def printCards():
+    # Print the current state of the deck in the terminal.
+    printCounter = 0
+    for card in cards:
+        print("Card number:", printCounter, card.suit, card.color, card.pile, card.value, card.visible)
+        printCounter+=1
+
+
+setupTable()
+
+printCards()
