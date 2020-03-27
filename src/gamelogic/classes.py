86ba6adb47f68a1d8bc9f130f42ecb5ddbee5e42
mj23000 <markusjacobsen@hotmail.com>
2020-03-27 18:33:18 +0100

    Formatting and small code adjustments

--- src/gamelogic/classes.py
+++ src/gamelogic/classes.py
@@ -3,9 +3,9 @@ import random
 
 NOCARDS = 52
 NOCARDS_PLATEAU = 28 # 1+2+3+4+5+6+7=28
+
 cards = []
 tableau_piles = []
-
 foundations_piles = []
 
 class Suit(Enum):
@@ -43,7 +43,6 @@ class Value(Enum):
     QUEEN = 12
     KING = 13
 
-
 class Visible(Enum):
     FALSE = 0
     TRUE = 1
@@ -57,14 +56,13 @@ class Playing_Card:
         self.visible = visible
     
     def getCardString(self):
-        if(self.visible == 1):
-            if (self.value.value < 10):
+        if self.visible == 1:
+            if self.value.value < 10:
                 return str(0) + str(self.value.value) + "," + str(self.suit.name)
             else:
                 return str(self.value.value) + "," + str(self.suit.name)
         else:
             return "###" 
-
 
 class tableauPile:
     def __init__(self, number):
@@ -111,32 +109,29 @@ def setupTable():
             currentCard = cards[card]
             currentCard.pile = Pile.TABLEAU 
             print("pile", pile, "card", cardnr) 
-
             # Make the top card in the pile visible.
             if cardnr == pile:
                 currentCard.visible = Visible.TRUE 
-
             newPile.Cards.append(cards[card])
             newPile.frontCard = cards[card]
-            
             card+=1
             print(newPile.Cards[-1].value, newPile.Cards[-1].suit, newPile.Cards[-1].visible)
-
         tableau_piles.append(newPile)
         newPile.frontCard.visible = 1
-        print("Size of the pile:", len(tableau_piles))
+        print("Size of the pile:", len(tableau_piles),"\n")
     # Make the rest of the cards the playing cards in the stockpile.
     #stock = stockPile()
+    
     for card in range(NOCARDS_PLATEAU, NOCARDS):
         cards[card].pile = Pile.STOCK
         stock.Cards.append(cards[card])
         cards[card].visible = 1
         stock.frontCard =  cards[card]
+
     #Initialize the Foundation piles
     for suit in Suit:
         newFoundationPile = foundationPile(suit)
         foundations_piles.append(newFoundationPile)
-
 
 def printCards():
     # Print the current state of the deck in the terminal.
@@ -149,19 +144,22 @@ def printCards():
 # Code runs here
 def printTable():
     # Print first line with Stock pile and the Foundation piles
+    print("The playing table looks like:\n")
     str = ""
-    if(stock.frontCard == None):
+    if stock.frontCard == None:
         str = "0,X     "
     else:
         str = stock.frontCard.getCardString() + "     "
+
     for i in range(len(foundations_piles)):
-        if (foundations_piles[i].frontCard == None):
+        if foundations_piles[i].frontCard == None:
             str = str + "0," + foundations_piles[i].suit.name + "  "
         else:
             str = str + foundations_piles[i].frontCard.getCardString() + " "
-    print(str)
-    print("")
+            
+    print(str,"\n")
     str = ""
+
     # Print Tableau piles
     for j in range(len(tableau_piles)):
         for pile in tableau_piles:
@@ -169,10 +167,10 @@ def printTable():
                str = str + pile.Cards[j].getCardString() + "  "
             else:
                 str = str + "     "
-        print(str)
+        print(str,)
         str = ""
-        
-
+    print()
+    
 setupTable()
 #printCards()
 printTable()
