d3462e932af2256de2fbfa8b172d039e8caff23b
mj23000 <markusjacobsen@hotmail.com>
2020-06-12 12:21:38 +0200

    Fixed shell command

--- src/CDIO_OPENCV-DNN/run.sh
+++ src/CDIO_OPENCV-DNN/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python detect_cards.py --config=yolocards_test.cfg --model=yolocards_28000.weights --classes=cards.names --width=1216 --height=1216 --input=http://192.168.31.97:8080/video --rgb
+python detect_cards.py --config=yolocards_test.cfg --model=yolocards_28000.weights --classes=cards.names --width=1216 --height=1216 --scale=0.00392 --input=http://192.168.31.97:8080/video --rgb
+
 
