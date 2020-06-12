010bd352bd8d7d3c5c0e134effbd5ab99a3555c5
mj23000 <markusjacobsen@hotmail.com>
2020-06-12 10:53:01 +0200

    Added initial opencv-dnn folder

---
+++ src/CDIO_OPENCV-DNN/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python detect_cards.py --config=yolocards_test.cfg --model=yolocards_28000.weights --classes=cards.names --width=1216 --height=1216 --input=http://192.168.31.97:8080/video --rgb
+
